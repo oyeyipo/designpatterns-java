40097daaa28eef964dce6cb33dec0d908741aa84
Olawale Oyeyipo <olaclassik@gmail.com>
2021-06-15 20:23:39 +0100

    Done with state

---
+++ src/designpatterns/state/define.md
@@ -0,0 +1,3 @@
+# The State Pattern
+
+**State pattern** allows an object to alter its behavior when its internal state changes. The object will appear to change its class.
---
+++ src/designpatterns/state/gumball/State.java
@@ -0,0 +1,13 @@
+package designpatterns.state.gumball;
+
+public interface State {
+    public void insertQuarter();
+
+    public void ejectQuarter();
+
+    public void turnCrank();
+
+    public void dispense();
+
+    public void refill();
+}
